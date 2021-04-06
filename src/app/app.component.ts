1096355821ca10d2f55dd702725d89e4684aa190
scrutycs <j-albr16@uni-muenster.de>
2021-04-06 10:15:32 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, Output} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'rote-ruebe';
+  @Output() title = 'rote-ruebe';
 }
