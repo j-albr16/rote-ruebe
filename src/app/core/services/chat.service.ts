76cc7f47d7588fe0152cb99dc677aa9cb943f733
FyndetNeo <65955494+FyndetNeo@users.noreply.github.com>
2021-04-23 15:14:57 +0200

    Chat Service Skeleton

---
+++ src/app/core/services/chat.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import User from '@core/models/user';
+import {Observable, Subject} from 'rxjs';
+import Message from '@core/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor() { }
+  /**
+   * @return Returns an Observable that emits the last Message of each given user in userList as a {User, Message} Object
+   */
+  getLatestMessage(userList: User[]): Observable<{user: User, message: Message}>{
+    throw Error('getLatestMessage in @core/services/chat.service is not implemented');
+    return;
+  }
+
+  /**
+   * @return Returns an {Observable, Subject} object.
+   * The Observable will emit requested Messages from given User in the order old -> new.
+   * With the Subject you can request more Messages by emitting the wanted number.
+   */
+  getMessages(user: User): {observable: Observable<Message>, subject: Subject<number>} {
+    throw Error('getMessages in @core/services/chat.service is not implemented');
+    return{observable: null, subject: null};
+  }
+
+  /**
+   * @return The returned Observable will emit new incoming Messages until unsubscribed
+   */
+  getNewMessages(): Observable<{ user: User, message: Message}>{
+    throw Error('getNewMessages in @core/services/chat.service is not implemented');
+    return;
+  }
+
+  /**
+   * @return Returns a {User, number} Object that describes the number of unread Messages in the chat with assosiated user.
+   * After the initial emit it will emit new UnreadMessages until unsubscribed
+   */
+  getUnreadMessages(): Observable<{user: User, amount: number}>{
+    throw Error('getUnreadMessages in @core/services/chat.service is not implemented');
+    return;
+  }
+
+  /**
+   * Marks every Message the device User has with the given User as read.
+   * Will instantly mark any new Message with given User as read as long as given Observable is not completed.
+   */
+  markMessagesAsRead(userObs: Observable<User>): void{
+    throw Error('markMessagesAsRead in @core/services/chat.service is not implemented');
+    return;
+  }
+
+  /**
+   * Sends a chat message to the Server
+   * @return the Observable will emit true if the Message is send successfully.
+   */
+  sendMessage(message: Message): Observable<boolean>{
+    throw Error('sendMessage in @core/services/chat.service is not implemented');
+    return;
+  }
+}
--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,5 +1,7 @@
-import { AuthService } from './auth.service';
+import { AuthService} from './auth.service';
+import { ChatService} from './chat.service';
 
-export const services: any[] = [AuthService];
+export const services: any[] = [AuthService, ChatService];
 
 export * from './auth.service';
+export * from './chat.service';
