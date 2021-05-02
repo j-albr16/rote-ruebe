b0cce2b8b2494ccff83910153d46102b48a73df8
scrutycs <j-albr16@uni-muenster.de>
2021-05-02 20:30:34 +0200

    Auth Page Design

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -23,18 +23,31 @@ export class AuthService {
   constructor(private http: AppHttpClient) {
   }
 
+  /**
+   * Retrieving Userid from authenticated User Model
+   */
   get userId(): string {
     return this.user.id;
   }
 
+  /**
+   * Checking Whether Auth Token is Available in Auth User Model
+   */
   get isAuth(): boolean {
     return !!this.user?.loginToken;
   }
 
+  /**
+   * Get Auth String for API Authentication
+   */
   getAuth(): string {
     return this.user.id + ':' + this.user.loginToken;
   }
 
+  /**
+   * Log In http Request. False => ErrorList. Valid => Auth User Model with Token
+   * @param userData email password
+   */
   public logIn(userData: LogInRequest): Observable<ErrorList | null> {
     return this.http.post<LogInResponse>(AuthRoutes.LogIn, userData).pipe(
       tap(data => {
@@ -48,6 +61,10 @@ export class AuthService {
     );
   }
 
+  /**
+   * Making API Call to create new User Model
+   * @param user User Data for new User. UserName, Password, ConfirmPassword, Email
+   */
   public singUp(user: SignUpRequest): Observable<ErrorList | null> {
     return this.http.post<null>(AuthRoutes.SignUp, user).pipe(
       catchError<null, Observable<ErrorList | null>>((err: ErrorBody) => {
@@ -58,6 +75,10 @@ export class AuthService {
     );
   }
 
+  /**
+   * API Call to Request new Password. Email will be send to User email Address
+   * @param user Email
+   */
   public requestResetPassword(user: RequestResetPasswordRequest): Observable<ErrorList | null> {
     return this.http.post<null>(AuthRoutes.RequestResetPassword, user).pipe(
       catchError<null, Observable<ErrorList | null>>((err: ErrorBody) => {
@@ -68,6 +89,10 @@ export class AuthService {
     );
   }
 
+  /**
+   * API Call to Reset Password
+   * @param user Password and Confirm Password.
+   */
   public resetPassword(user: ResetPasswordRequest): Observable<ErrorList | null> {
     return this.http.post<null>(AuthRoutes.ResetPassword, user).pipe(
       catchError<null, Observable<ErrorList | null>>((err: ErrorBody) => {
